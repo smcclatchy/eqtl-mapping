f6b6745c283adeb0108622b8357ac65587fc1ea1
Sue McClatchy <susan.mcclatchy@jax.org>
2024-12-10 08:18:26 -0500

    create rankz.R script and source it from map one qtl episode

---
+++ episodes/code/rankz.R
@@ -0,0 +1,10 @@
+################################################################################
+# The rankz() function forces data into a normal distribution.
+################################################################################
+
+rankZ = function(x) {
+  x = rank(x,
+           na.last     = "keep",
+           ties.method = "average") / (sum(!is.na(x)) + 1)
+  return(qnorm(x))
+} 
